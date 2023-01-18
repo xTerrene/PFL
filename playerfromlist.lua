86e26bd4b3908a41573f1ac768e23bcc1036ff72
EarthToAccess <oldmonkey333@gmail.com>
2023-01-18 18:44:09 -0500

    code optimizations and restructuring of how data returns

--- playerfromlist.lua
+++ playerfromlist.lua
@@ -3,7 +3,7 @@ local pfl = {}
 -- created by 0xEARTH
 -- version v1.2
 
-function pfl:findp(instr,dtype)
+function pfl:findp(list,dtype)
 
 	local players = game.Players
 	
@@ -11,92 +11,75 @@ function pfl:findp(instr,dtype)
 		dtype = 3
 	end
 	
-	if instr == nil then
-		instr = "N/A"
+	if list == nil then
+		return nil
 	end	
 
 	local t = {}
-	
-	if dtype == 2 then
-		if instr == "N/A" then return instr end
 
-		for str in string.gmatch(instr, "([^%s+]+)") do
-			table.insert(t, str)
-		end
+	for str in string.gmatch(list, "([^%s+]+)") do
+		table.insert(t, str)
+	end
+
+	if dtype == 1 then
 
 		local pt = {}
 
 		for i,v in pairs(players:GetPlayers()) do
 			if table.find(t,v.Name) then
 				table.insert(pt,v.Name)
+			elseif table.find(t,tostring(v.UserId)) then
+				table.insert(pt,v.UserId)
 			end
-			
-			if table.find(t,tostring(v.UserId)) then
-				local uid = v.UserId
-				local name = players:GetNameFromUserIdAsync(v.UserId)
-				local plr = {["UserId"] = uid, ["Name"] = name}
+		end
+
+		if #pt == 0 then
+			return nil
+		else
+			return unpack(pt)
+		end
+	end
+	
+	if dtype == 2 then
+
+		local pt = {}
+		local plr
+		local uid
+		local name
+
+		for i,v in pairs(players:GetPlayers()) do
+			if table.find(t,v.Name) or table.find(t,tostring(v.UserId)) then
+				uid = v.UserId
+				name = players:GetNameFromUserIdAsync(v.UserId)
+				plr = {["UserId"] = uid, ["Name"] = name}
 				table.insert(pt,plr)
 			end
 		end
 
 		if #pt == 0 then
-			return "N/A"
+			return nil
 		else
 			return pt
 		end
 	end
 		
 	if dtype == 3 then
-		
-		if instr == "N/A" then return instr end
 
 		local plr
-		
-		for str in string.gmatch(instr, "([^%s+]+)") do
-			table.insert(t, str)
-		end
+		local uid
+		local name
 	
 		for i,v in pairs(players:GetPlayers()) do
-			if table.find(t,v.Name) then
-				plr = v.Name
-				return plr
-			end
-
-			if table.find(t,tostring(v.UserId)) then
-				local uid = v.UserId
-				local name = players:GetNameFromUserIdAsync(v.UserId)
-				local plr = {["UserId"] = uid, ["Name"] = name}
+			if table.find(t,v.Name) or table.find(t,tostring(v.UserId)) then
+				uid = v.UserId
+				name = players:GetNameFromUserIdAsync(v.UserId)
+				plr = {["UserId"] = uid, ["Name"] = name}
 				return plr
 			end
 		end
-		return "N/A"
+		return nil
 	end
 	
-	if dtype == 1 then
-		if instr == "N/A" then return instr end
-
-		for str in string.gmatch(instr, "([^%s+]+)") do
-			table.insert(t, str)
-		end
-
-		local pt = {}
-
-		for i,v in pairs(players:GetPlayers()) do
-			if table.find(t,v.Name) then
-				table.insert(pt,v.Name)
-			end
-			
-			if table.find(t,tostring(v.UserId)) then
-				table.insert(pt,v.UserId)
-			end
-		end
-
-		if #pt == 0 then
-			return "N/A"
-		else
-			return unpack(pt)
-		end
-	end
 end
 
 return pfl
